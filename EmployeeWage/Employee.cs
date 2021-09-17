cb5b02865149f68d0699dcc53ed559038fcb3127
chaitanya4294 <chaitanya.4294@gmail.com>
2021-09-17 19:56:35 +0530

    [Chaitanya] Add: Added limitations in Monthly employee wage calculation

--- EmployeeWage/Employee.cs
+++ EmployeeWage/Employee.cs
@@ -9,23 +9,30 @@ namespace EmployeeWage
         public int MonthlyWage()
         {
             // Define constants
-            const int IS_FULL_TIME = 1, IS_PART_TIME = 2, FULL_DAY_HRS = 8, PART_TIME_HRS = 4, WAGE_PER_HR = 20, MAX_WORKING_DAYS = 20;
+            const int IS_FULL_TIME = 1, IS_PART_TIME = 2, FULL_DAY_HRS = 8, PART_TIME_HRS = 4, WAGE_PER_HR = 20, MAX_WORKING_DAYS = 20, MAX_WORKING_HRS = 100;
             // Define variables
-            int empHours = 0;
+            int empHours = 0, workingDays = 1;
             // Create random number generator
             Random random = new Random();
             // Calculate Total hours worked in a month
-            for (int workingDays = 1; workingDays < MAX_WORKING_DAYS; workingDays++)
+            while ( workingDays <= MAX_WORKING_DAYS &  empHours<100)
             {
+                workingDays++;
                 int empCheck = random.Next(3);
                 // Check employee attendance and calculate total working hours of employee
                 switch (empCheck)
                 {
                     case IS_FULL_TIME:
-                        empHours += FULL_DAY_HRS;
+                        if (empHours > 92)
+                            empHours = 100;
+                        else
+                            empHours += FULL_DAY_HRS;
                         break;
                     case IS_PART_TIME:
-                        empHours += PART_TIME_HRS;
+                        if (empHours > 96)
+                            empHours = 100;
+                        else
+                            empHours += PART_TIME_HRS;
                         break;
                     default:
                         break;
