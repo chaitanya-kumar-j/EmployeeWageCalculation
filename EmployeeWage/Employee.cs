87e62cce2f7d97d1a654987d725072eef479d29c
chaitanya4294 <chaitanya.4294@gmail.com>
2021-09-17 19:07:33 +0530

    [Chaitanya] Add: Added Part time employee wage calculation

--- EmployeeWage/Employee.cs
+++ EmployeeWage/Employee.cs
@@ -9,15 +9,17 @@ namespace EmployeeWage
         public int DailyWage()
         {
             // Define constants
-            const int IS_PRESENT = 1, FULL_DAY_HRS = 8, WAGE_PER_HR = 20;
+            const int IS_FULL_TIME = 1, IS_PART_TIME = 2, FULL_DAY_HRS = 8, PART_TIME_HRS = 4, WAGE_PER_HR = 20;
             // Define variables
             int empHours = 0;
             // Create random number generator
             Random random = new Random();
-            int empCheck = random.Next(2);
-            // Check attendance of employee
-            if (empCheck == IS_PRESENT)
+            int empCheck = random.Next(3);
+            // Check employee attendance and calculate total working hours of employee
+            if (empCheck == IS_FULL_TIME)
                 empHours += FULL_DAY_HRS;
+            if (empCheck == IS_PART_TIME)
+                empHours += PART_TIME_HRS;
             // Calculate the wages
             int dailyWage = empHours * WAGE_PER_HR;
             return dailyWage;
